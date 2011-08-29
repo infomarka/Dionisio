6e2ae4c11a1e957050110d223aee3eb8b44be260
Joao Felipe <joao@joao-R590.(none)>
2011-08-29 11:32:24 -0300

    commit inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dionisio::Application.config.secret_token = 'd9b33f5e7cba9a3aa426ee7fa5d7cabaa7b6b79730836f0d2ef52ef75c5b38a035b68872e7ae8bbc22bd95db918ba0ec679577210a63d1b0b656547caed012e6'
