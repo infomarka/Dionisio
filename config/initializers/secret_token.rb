2b8c1786ec7fe9aa8cec039a0a7a72ec8fce2a23
Infomarka <noob.infomarka@gmail.com>
2011-09-08 14:35:03 -0300

    rails 3.1

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dionisio::Application.config.secret_token = '05aed497b8939baf732dbb68d7f8aca19770d16c7fdccf32eec56c83cad9ab87ee6456fce5e0ab7009c81ac65122dd88e3e03128cbf6c234e84c31ee59f9cda3'
